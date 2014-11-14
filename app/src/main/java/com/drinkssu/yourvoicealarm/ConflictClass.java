871d526ee0e3a7b04b8aacad356ba0c908640e9c
JungJungyu <monibu1548@naver.com>
2014-11-15 04:10:14 +0900

    conflict 진규

--- app/src/main/java/com/drinkssu/yourvoicealarm/ConflictClass.java
+++ app/src/main/java/com/drinkssu/yourvoicealarm/ConflictClass.java
@@ -7,9 +7,20 @@ public class ConflictClass {
     //Conflict 1
     //Conflict 2
     //Conflict 3
-    //Conflict 4
+    //Conflict 4 glglgldpdpdpgps
+    void erer()
+    {}
+
     //Conflict 5
     //Conflict 6
+
+    @Override
+    public boolean equals(Object o) {
+         int a;
+
+        return true;
+    }
+
     //Conflict 7
     //Conflict 8
     //Conflict 9
