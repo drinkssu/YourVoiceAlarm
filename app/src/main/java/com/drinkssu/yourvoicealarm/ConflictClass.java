94caf1b765affef8ae0ef7599e80f2bbef7b1f03
FlaShilver <jongchan@navercorp.com>
2014-11-15 03:57:24 +0900

    Conflict 실험을 위한 예시 파일.

---
+++ app/src/main/java/com/drinkssu/yourvoicealarm/ConflictClass.java
@@ -0,0 +1,17 @@
+package com.drinkssu.yourvoicealarm;
+
+/**
+ * Created by FlaShilver on 14. 11. 15..
+ */
+public class ConflictClass {
+    //Conflict 1
+    //Conflict 2
+    //Conflict 3
+    //Conflict 4
+    //Conflict 5
+    //Conflict 6
+    //Conflict 7
+    //Conflict 8
+    //Conflict 9
+    //Conflict 10
+}
