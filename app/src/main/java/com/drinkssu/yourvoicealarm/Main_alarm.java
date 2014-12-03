53bcf08e308771746b0552e4de420bf0890667d7
KooSeongMin <ababqq@naver.com>
2014-12-04 04:58:08 +0900

    초기 유저정보 셋팅

--- app/src/main/java/com/drinkssu/yourvoicealarm/Main_alarm.java
+++ app/src/main/java/com/drinkssu/yourvoicealarm/Main_alarm.java
@@ -4,13 +4,25 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by KSM on 2014-11-17.
@@ -33,7 +45,7 @@ public class Main_alarm extends Activity {
     public final static int FRAGMENT_ONE = 0;
     public final static int FRAGMENT_TWO = 1;
     public final static int FRAGMENT_THREE = 2;
-
+    private File mk_user_info = Environment.getExternalStorageDirectory();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -126,6 +138,44 @@ public class Main_alarm extends Activity {
                 }
             }
         });
+        
+        set_user_info();
+        
+    }
+
+    private void set_user_info() {
+        File path = null;
+        path = new File(mk_user_info.getAbsolutePath() + "/Android/data/com.drinkssu.yourvoicealarm/YourVoiceAlarm/user_info.txt"); // 디렉토리 만들어쥬는부분!!!
+        if (!path.exists())                   // 디렉토리 없으면 만들어요!!
+        {
+            // 디렉토리가 존재하지 않으면 디렉토리 생성
+            try {
+                String settingStr = "0";
+                FileOutputStream fos = new FileOutputStream(path);
+                fos.write(settingStr.getBytes());
+                fos.close();
+                Intent intent = new Intent(this, user_info.class);
+                startActivity(intent);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            try {
+                FileInputStream fis = new FileInputStream(path);
+                BufferedReader bufferReader = new BufferedReader(new InputStreamReader(fis));
+                String temp="";
+                temp = bufferReader.readLine();
+                if(temp.toString().equals("0") ) {
+                    Intent intent = new Intent(this, user_info.class);
+                    startActivity(intent);
+                }
+                Log.v(null,""+temp);
+            } catch (Exception e) {}
+        }
     }
     protected void view_frag()
     {
